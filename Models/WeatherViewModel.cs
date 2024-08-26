be5939510b3977ad26ea4366dc2a10f535553e2e
ilginucum <ilginucum@sabanciuniv.edu>
2024-08-26 12:05:12 +0300

    completed frontend

--- Models/WeatherViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Weather_App.Models
-{
-    public class WeatherViewModel
-    {
-        public WeatherForecast CurrentWeather { get; set; }
-        public List<WeatherForecast> WeeklyForecast { get; set; }
-        public string SearchCity { get; set; }
-    }
-}
--- Options/WeatherApiOptions.cs
+++
@@ -1,7 +0,0 @@
-namespace Weather_App.Options{
-public class WeatherApiOptions
-{
-    public string BaseUrl { get; set; }
-    public string ApiKey { get; set; }
-}
-}
